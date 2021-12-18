e5aa3de324fa3838d7a18249f23dec939bc4190e
baalique <baalique@gmail.com>
2021-12-18 19:38:21 +0700

    Add "Manhattan Distance" solution

---
+++ 6_kyu/manhattan_distance.hs
@@ -0,0 +1,6 @@
+-- https://www.codewars.com/kata/52998bf8caa22d98b800003a
+
+module Manhattan where
+
+manhattanDistance :: (Int, Int) -> (Int, Int) -> Int
+manhattanDistance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
